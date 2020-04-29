f336a23fcf6fd540a2c126d4c34f0eba3553ef36
Leyi Cai <leyic@andrew.cmu.edu>
2020-04-29 18:36:16 -0400

    Fix minor bugs

--- Plume/caregiver/views.py
+++ Plume/caregiver/views.py
@@ -49,6 +49,7 @@ game_dict['f7c65423483c420c86569b8aedcfae0f'] = 'WordSearch'
 game_dict['c263ba76dc1543c08a151833443fc6b1'] = 'TileMatching'
 game_dict['3'] = 'BrownPeterson'
 
+
 def home(request):
     profile = request.user.caregiverprofile
     patient = profile.patient
@@ -78,7 +79,8 @@ def home(request):
 
     context['patient'] = patient
     return render(request, 'caregiver/index.html', context)
-    
+
+
 def todo(request):
     print(request.user)
     if request.method == 'POST':
@@ -90,12 +92,14 @@ def todo(request):
             end = form.cleaned_data['end_time']
             patient = request.user.caregiverprofile.patient
             print("patient name is: " + patient.user.username)
-            newevent = CalendarEvent(patient=patient, description=description, date=date, start=start, end=end)
+            newevent = CalendarEvent(
+                patient=patient, description=description, date=date, start=start, end=end)
             newevent.save()
             return redirect("/caregiver/todo")
     form = TodoEventForm()
     context = {'form': form}
     return render(request, 'caregiver/todo.html', context)
+
 
 def calendar(request):
     if request.method == 'POST':
@@ -107,7 +111,8 @@ def calendar(request):
             end = form.cleaned_data['end_time']
             patient = request.user.caregiverprofile.patient
             print("patient name is: " + patient.user.username)
-            newevent = CalendarEvent(patient=patient, description=description, date=date, start=start, end=end)
+            newevent = CalendarEvent(
+                patient=patient, description=description, date=date, start=start, end=end)
             newevent.save()
             return redirect("/caregiver/calendar")
     form = CalendarEventForm()
@@ -116,10 +121,17 @@ def calendar(request):
 
 
 def metrics(request):
-    return render(request, 'caregiver/metrics.html')
+    profile = request.user.caregiverprofile
+    patient = profile.patient
+    # print(profile.user.username)
+    context = {}
+    context['patient'] = patient
+    return render(request, 'caregiver/metrics.html', context)
+
 
 def games(request):
     return render(request, 'caregiver/game_record.html')
+
 
 def exercises(request):
     return render(request, 'caregiver/exercises.html')
@@ -308,7 +320,6 @@ def survey(request):
         print("user is not authenticated")
         return render(request, 'caregiver/survey.html', context)
 
-    
     if caregiver.patient.survey is None:
         survey = Survey()
         survey.save()
@@ -367,7 +378,7 @@ def survey(request):
     print(caregiver.patient.survey)
 
     context['newuser'] = caregiver
-    
+
     # Validates the form.
     # if not form.is_valid():
     #     print("form is not valid")
@@ -376,13 +387,15 @@ def survey(request):
     print(caregiver.patient.survey)
     return render(request, 'caregiver/survey.html', {'form': form, 'alert_flag': True, 'newuser': caregiver})
 
+
 def login(request):
     return render(request, 'caregiver/login.html')
 
 
 def register(request):
     return render(request, 'caregiver/register.html')
-    
+
+
 def getevents(request):
     patient = request.user.caregiverprofile.patient
     all_events = CalendarEvent.objects.filter(patient=patient)
@@ -420,10 +433,11 @@ def metric_display(request):
     if len(retrieved_emotion) > 30:
         retrieved_emotion = retrieved_emotion[len(
             retrieved_emotion) - 30: len(retrieved_emotion)]
-    
+
     before_n_days = []
     for i in range(1, 30 + 1)[::-1]:
-        before_n_days.append(str(datetime.date.today() - datetime.timedelta(days=i)))
+        before_n_days.append(
+            str(datetime.date.today() - datetime.timedelta(days=i)))
 
     # init mood dict
     diction["mood"] = {"time": before_n_days, "yvalue": {}}
@@ -453,7 +467,6 @@ def metric_display(request):
 
     # print(diction["mood"])
     # print("==================================================================")
-
 
     # Biometric:
     # Issue: a) currenlty no tremor data
@@ -517,6 +530,7 @@ def metric_display(request):
     # print("==================================================================")
 
     return HttpResponse(json.dumps(diction), content_type='application/json')
+
 
 def game_metric(request):
     db = get_db()
@@ -602,7 +616,7 @@ def game_metric(request):
 def view_general(request):
     db = get_db()
     # patient_id = '10000000-0000-0000-0000-000000000000'
-    
+
     caregiver = CaregiverProfile.objects.get(user=request.user)
     patient = caregiver.patient
     print(patient.user.id)
